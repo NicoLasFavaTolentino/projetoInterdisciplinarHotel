be1df8e7854634933c029d780df2c9e9d06a1519
Alexandre Massao <alexandremsubs@gmail.com>
2022-10-27 12:10:41 -0300

    adicionada função para editar clientes já existentes
    2 funcões adicionadas: indiceClienteCpf e verficarExistenciaCpf

--- src/projeto/Main.java
+++ src/projeto/Main.java
@@ -1,8 +1,36 @@
 package projeto;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+	static int indiceClienteCpf(Hotel hotel, String cpf){
+		int indice = 0;
+		ArrayList<Cliente> clientes = hotel.getClientes();
+		
+		for (Cliente cliente : clientes){
+			if (cliente.getCpf().equals(cpf)){
+				indice = clientes.indexOf(cliente);
+			}
+		}
+		
+		return indice;
+	}
+
+	static boolean verificarExistenciaCpf(Hotel hotel, String cpf){
+		boolean existe = false;
+		ArrayList<Cliente> clientes = hotel.getClientes();
+
+		for (Cliente cliente : clientes){
+			if (cliente.getCpf().equals(cpf)){
+				existe = true;
+			}
+		}
+		
+		return existe;
+	}
+
 	public static void main(String[] args) {
 		Scanner leia = new Scanner (System.in);
 		leia.useDelimiter("\\R"); //https://stackoverflow.com/questions/69680170/scanner-skipping-my-nextline-statement-in-constructor
@@ -15,12 +43,14 @@ public class Main {
 			System.out.println("1 - Adicionar Cliente" + "\n" +
 					"2 - Remover Cliente" + "\n" +
 					"3 - Pesquisar Cliente" + "\n" +
-					"4 - Exibir Todos os Clientes");
+					"4 - Editar Cliente" + "\n" +
+					"5- Exibir Todos os Clientes");
 			
 			System.out.print("Sua escolha: ");
 			escolha = leia.next().toUpperCase();
 			
 			switch(escolha) {
+				//adicionar cliente
 				case "1":
 					System.out.print("Nome: ");
 					nome = leia.next();
@@ -31,14 +61,15 @@ public class Main {
 					System.out.print("Data Nascimento: ");
 					dataNascimento = leia.next();
 					
-					System.out.print("Endere�o: ");
+					System.out.print("Endereço: ");
 					endereco = leia.next();
 					
 					Cliente cliente = new Cliente(nome, cpf, dataNascimento, endereco);
 					hotel.adicionarCliente(cliente);
 
 					break;
-					
+
+					//remover cliente
 				case "2":
 					System.out.print("Insira o CPF do cliente a ser removido: ");
 					cpf = leia.next();
@@ -59,6 +90,7 @@ public class Main {
 					
 					break;
 
+					//pesquisar cliente
 				case "3":
 					System.out.print("CPF a ser procurado: ");
 					cpf = leia.next();
@@ -67,8 +99,50 @@ public class Main {
 					hotel.printClienteComCpf(cpf);
 
 					break;
-					
+
+					//editar cliente
 				case "4":
+					System.out.print("CPF: ");
+					cpf = leia.next();
+					boolean cpfExiste = verificarExistenciaCpf(hotel, cpf);
+
+					if (cpfExiste){
+						indice = indiceClienteCpf(hotel, cpf);
+
+						System.out.println("Encontrado: ");
+						hotel.printClienteComCpf(cpf);
+
+						System.out.print("Editar? S/N");
+						opcao = leia.next().toUpperCase();
+
+						if (opcao.equals("S")){
+							System.out.print("Nome: ");
+							nome = leia.next();
+							System.out.print("CPF: ");
+							cpf = leia.next();
+							System.out.print("Data de Nascimento: ");
+							dataNascimento = leia.next();
+							System.out.print("Endereço: ");
+							endereco = leia.next();
+
+							hotel.editarCliente(indice,
+									nome,
+									cpf,
+									dataNascimento,
+									endereco);
+
+						}else{
+							System.out.println("Cancelado!");
+						}
+
+					}else {
+						System.out.println("CPF não encontrado");
+					}
+
+					break;
+
+					//exibir clientes
+				case "5":
 					System.out.println("Todos os clientes: ");
 					hotel.printClientes();
 					
